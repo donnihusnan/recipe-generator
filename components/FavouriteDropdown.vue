19f66b5f0c885c4db0f2701c1772a78dbc5627c3
donnihusnan <donnihusnan@gmail.com>
2025-05-09 20:52:42 +0700

    add dropdown

---
+++ components/FavouriteDropdown.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+type Recipe = {
+  id: number;
+  title: string;
+  ingredients: string[];
+  instructions: string;
+};
+
+const { favorites } = defineProps<{
+  favorites: Recipe[];
+}>();
+
+const items = computed(() => [
+  {
+    label: 'Your Favourites',
+    slot: 'header',
+  },
+  ...favorites.map((r: Recipe) => ({
+    label: r.title,
+    click: '',
+  })),
+]);
+</script>
+
+<template>
+  <UDropdownMenu :items="items">
+    <UButton icon="i-lucide-menu" color="neutral" variant="outline" />
+  </UDropdownMenu>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,4 @@
-<template>
-  <main
-    class="min-h-screen mx-auto p-8 bg-gradient-to-b from-white to-gray-100 sm:w-full md:w-full lg:w-[50%]"
-  >
-    <h1 class="text-3xl font-bold mb-6">Build Your Recipe</h1>
-
-    <IngredientSelector
-      :ingredients="allIngredients"
-      v-model="selectedIngredients"
-    />
-
-    <button
-      @click="findRecipes"
-      class="mt-8 px-6 py-3 bg-pink-500 text-white rounded-lg hover:bg-pink-600 transition"
-    >
-      Find Recipes
-    </button>
-
-    <div v-if="recipes.length" class="mt-12 space-y-6">
-      <h2 class="text-2xl font-semibold">✨Suggested Recipes</h2>
-
-      <div
-        v-for="recipe in recipes"
-        :key="recipe.id"
-        class="bg-white p-4 rounded-md shadow"
-      >
-        <h3 class="text-xl font-bold mb-2">{{ recipe.title }}</h3>
-        <p class="text-md font-bold text-gray-700">
-          Ingredients:
-          <span class="font-medium"> {{ recipe.ingredients.join(', ') }}</span>
-        </p>
-        <p class="text-gray-700 font-bold">
-          Simple Instruction:
-          <span class="font-medium">{{ recipe.instructions }}</span>
-        </p>
-
-        <button
-          @click="saveToFavorites(recipe)"
-          class="mt-2 bg-green-700 text-white px-3 py-1 rounded hover:bg-green-800 transition"
-        >
-          💾 Save to Favorites
-        </button>
-      </div>
-    </div>
-  </main>
-</template>
-
+#TODO Remove Favorites [] #TODO Add Favorites Page []
 <script setup lang="ts">
 const allIngredients = [
   'Beef',
@@ -116,3 +70,55 @@ const saveToFavorites = (recipe: Recipe) => {
   }
 };
 </script>
+
+<template>
+  <main
+    class="min-h-screen mx-auto p-8 bg-gradient-to-b from-white to-gray-100 sm:w-full md:w-full lg:w-[50%]"
+  >
+    <div class="flex justify-between">
+      <h1 class="text-3xl font-bold mb-6">Build Your Recipe</h1>
+      <div>
+        <FavouriteDropdown :favorites="favorites" />
+      </div>
+    </div>
+
+    <IngredientSelector
+      :ingredients="allIngredients"
+      v-model="selectedIngredients"
+    />
+
+    <button
+      @click="findRecipes"
+      class="mt-8 px-6 py-3 bg-pink-500 text-white rounded-lg hover:bg-pink-600 transition"
+    >
+      Find Recipes
+    </button>
+
+    <div v-if="recipes.length" class="mt-12 space-y-6">
+      <h2 class="text-2xl font-semibold">✨Suggested Recipes</h2>
+
+      <div
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        class="bg-white p-4 rounded-md shadow"
+      >
+        <h3 class="text-xl font-bold mb-2">{{ recipe.title }}</h3>
+        <p class="text-md font-bold text-gray-700">
+          Ingredients:
+          <span class="font-medium"> {{ recipe.ingredients.join(', ') }}</span>
+        </p>
+        <p class="text-gray-700 font-bold">
+          Simple Instruction:
+          <span class="font-medium">{{ recipe.instructions }}</span>
+        </p>
+
+        <button
+          @click="saveToFavorites(recipe)"
+          class="mt-2 bg-green-700 text-white px-3 py-1 rounded hover:bg-green-800 transition"
+        >
+          💾 Save to Favorites
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
